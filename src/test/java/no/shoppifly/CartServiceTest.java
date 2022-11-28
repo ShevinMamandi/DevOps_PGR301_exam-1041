8a2821b4c5b7f1124b44555fa137481ec179bf86
Shevin Mamandi <shevin1987@gmail.com>
2022-11-28 14:35:18 +0100

    fiksed unittest and now pushing to testing pipline

--- src/test/java/no/shoppifly/CartServiceTest.java
+++ src/test/java/no/shoppifly/CartServiceTest.java
@@ -10,14 +10,21 @@ public class CartServiceTest {
     public void shouldRemoveCartAfterCheckout() {
 
         CartService service = new NaiveCartImpl();
+        //added cart1
         Cart theCart = Cart.builder().build();
         service.update(theCart);
         assertEquals(1, service.getAllsCarts().size());
+
+        //added cart2
+        Cart theCart2 = Cart.builder().build();
+        service.update(theCart2);
+        assertEquals(2, service.getAllsCarts().size());
+
         String orderId = service.checkout(theCart);
         assertNotNull(orderId);
 
         // Jim; This must be wrong, right? Shouldn't the cart be removed after checkout
-        assertEquals(100, service.getAllsCarts().size());
+        assertEquals(1, service.getAllsCarts().size());
     }
 
 }
