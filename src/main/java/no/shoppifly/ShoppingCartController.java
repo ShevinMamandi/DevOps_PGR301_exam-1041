2a0b64f7f821dc1ac0506f0c8e151360ce4345db
Shevin Mamandi <shevin1987@gmail.com>
2022-11-29 18:43:23 +0100

    fixed den tredje graphen med antall checkout

--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -2,12 +2,14 @@ package no.shoppifly;
 
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.*;
 import io.micrometer.core.instrument.Gauge;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +20,7 @@ public class ShoppingCartController implements ApplicationListener<ApplicationRe
     @Autowired
     private final CartService cartService;
     private HashMap<String, Cart> theCart = new HashMap();
+    private Timer productTimer;
     private MeterRegistry meterRegistry;
 
 
@@ -25,6 +28,7 @@ public class ShoppingCartController implements ApplicationListener<ApplicationRe
     public ShoppingCartController(CartService cartService, MeterRegistry meterRegistry) {
         this.cartService = cartService;
         this.meterRegistry = meterRegistry;
+        productTimer = meterRegistry.timer("checkout_latency");
     }
 
     @GetMapping(path = "/cart/{id}")
@@ -37,12 +41,18 @@ public class ShoppingCartController implements ApplicationListener<ApplicationRe
      *
      * @return an order ID
      */
+
     @Timed
     @PostMapping(path = "/cart/checkout")
     public String checkout(@RequestBody Cart cart) {
+        long startTime = System.currentTimeMillis();
         meterRegistry.counter("delete_cart").increment();
         String result = cartService.checkout(cart);
-        theCart.remove(cart.getId());
+        // record time to execute the method
+        productTimer.record(Duration
+                .ofMillis(System.currentTimeMillis()
+                        - startTime));
+        theCart.put(cart.getId(), cart);
         return result;
     }
 
@@ -56,7 +66,7 @@ public class ShoppingCartController implements ApplicationListener<ApplicationRe
     @PostMapping(path = "/cart")
     public Cart updateCart(@RequestBody Cart cart) {
         meterRegistry.counter("create_cart").increment();
-        theCart.put(cart.getId(), cart);
+        //theCart.put(cart.getId(), cart);
         return cartService.update(cart);
     }
 
@@ -74,13 +84,15 @@ public class ShoppingCartController implements ApplicationListener<ApplicationRe
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
 
         // Denne meter-typen "Gauge" rapporterer hvor mye penger som totalt finnes i banken
-        Gauge.builder("carts", theCart,
-                        b -> b.values().size()).register(meterRegistry);
-                        
+        Gauge.builder("carts", cartService,
+                        b -> b.getAllsCarts().size()).register(meterRegistry);
         
         Gauge.builder("cartsvalue", cartService,
                         b -> b.total())
                 .register(meterRegistry);
+
+        Gauge.builder("checkouts", theCart,
+                b -> b.values().size()).register(meterRegistry);
     }
 
 }
